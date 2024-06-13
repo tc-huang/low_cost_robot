f0647a7604a76d8f886e1c61033a0a0ed314f5d6
YS-Huang <a43414786@gmail.com>
2024-06-13 12:34:25 +0800

    Fix same position-xpos bug

--- simulation.py
+++ simulation.py
@@ -7,78 +7,90 @@ from simulation.interface import SimulatedRobot
 import numpy as np
 import pickle as pkl
 
-m = mujoco.MjModel.from_xml_path('simulation/low_cost_robot/scene.xml')
-d = mujoco.MjData(m)
+class SimulatedRobotClass:
 
-r = SimulatedRobot(m, d)
+  def __init__(self) -> None:
+    self.m = mujoco.MjModel.from_xml_path('simulation/low_cost_robot/scene.xml')
+    self.d = mujoco.MjData(self.m)
+    self.r = SimulatedRobot(self.m, self.d)
+    self.cnt = 0
+    self.workspace = []
+    self.workspace = self.read_pkl('workspace.pkl')
+    self.init_pos_idx, self.target_xpos_idx, self.init_pos, self.target_xpos = self.get_simulation_position_xpos()
 
-def random_gen_pos():
-  rst = np.random.uniform(-3.14158 / 2, 3.14158 / 2, 6)
-  # rst[2] = np.random.uniform(-3.14158 / 2, 3.14158 / 2)
-  rst[4] = 0
-  rst[5] = 0
-  return rst
-
-cnt = 0
-workspace = []
+  def random_gen_pos(self):
+    rst = np.random.uniform(-3.14158 / 2, 3.14158 / 2, 6)
+    # rst[2] = np.random.uniform(-3.14158 / 2, 3.14158 / 2)
+    rst[4] = 0
+    rst[5] = 0
+    return rst
   
-
-# with open('workspace.pkl', 'rb') as f:
-#   workspace = pkl.load(f)
-
-# init_pos_idx = np.random.randint(0, len(workspace))
-# target_xpos_idx = np.random.randint(0, len(workspace))
-
-
-# init_pos = workspace[init_pos_idx]['pos']
-# target_xpos = workspace[target_xpos_idx]['TCP']
-
-# for i in workspace:
-#   print(i['pos'], i['TCP'])
-  
-# exit()
-
-# print(init_pos_idx, init_pos)
-# print(target_xpos_idx, target_xpos)
-
-with mujoco.viewer.launch_passive(m, d) as viewer:
-  start = time.time()
-  while viewer.is_running():
-    
-    
-    if(cnt % 5000 == 0):
-      if(cnt != 0):
-        workspace.append(
+  def random_gen_position_xpos(self):
+    if(self.cnt % 2000 == 0):
+      if(self.cnt != 0):
+        self.workspace.append(
           {
-            'pos': r.read_position(),
-            'TCP': r.read_ee_pos()
+            'pos': self.r.read_position().copy(),
+            'TCP': self.r.read_ee_pos().copy()
           }
         )
       
         with open('workspace.pkl', 'wb') as f:
-          print(workspace[-1])
-          pkl.dump(workspace, f)
-          if(len(workspace) == 100):
+          print(self.workspace[-1])
+          pkl.dump(self.workspace, f)
+          if(len(self.workspace) == 100):
             exit()
           
-      cnt = 0
-      pos = random_gen_pos()
-    cnt += 1
-    r.set_target_pos(pos)
-    
-    
-    # if(cnt < 1000):
-    #   r.set_target_pos(init_pos)
-    # else:
-    #   r.set_target_pos(r.inverse_kinematics(target_xpos))
-    # cnt += 1
-    # print(cnt,end = '\r')
-    
-    
-    step_start = time.time()
-    mujoco.mj_step(m, d)
-    viewer.sync()
-    # Rudimentary time keeping, will drift relative to wall clock.
-    time_until_next_step = m.opt.timestep - (time.time() - step_start)
-    if time_until_next_step > 0:
-      time.sleep(time_until_next_step)
+      self.cnt = 0
+      self.pos = self.random_gen_pos()
+    self.cnt += 1
+    print(self.cnt, end = '\r')
+    self.r.set_target_pos(self.pos)
+
+  def read_pkl(self, file_names):
+    with open(file_names, 'rb') as f:
+      workspace = pkl.load(f)  
+    return workspace
+
+  def get_simulation_position_xpos(self):
+    init_pos_idx = np.random.randint(0, len(self.workspace))
+    target_xpos_idx = np.random.randint(0, len(self.workspace))
+    init_pos = self.workspace[init_pos_idx]['pos']
+    target_xpos = self.workspace[target_xpos_idx]['TCP']
+
+    for i in self.workspace:
+      print(i['pos'], i['TCP'])
+    # exit()
+
+    print(init_pos_idx, init_pos)
+    print(target_xpos_idx, target_xpos)
+
+    return init_pos_idx, target_xpos_idx, init_pos, target_xpos
+
+  def run(self):
+    with mujoco.viewer.launch_passive(self.m, self.d) as viewer:
+      start = time.time()
+      while viewer.is_running():
+        
+        
+        # self.random_gen_position_xpos()
+        
+        if(self.cnt < 1000):
+          self.r.set_target_pos(self.init_pos)
+        else:
+          self.r.set_target_pos(self.r.inverse_kinematics(self.target_xpos))
+        self.cnt += 1
+        print(self.cnt, np.subtract(self.r.read_ee_pos(), self.target_xpos),end = '\r')
+        
+        
+        step_start = time.time()
+        mujoco.mj_step(self.m, self.d)
+        viewer.sync()
+        # Rudimentary time keeping, will drift relative to wall clock.
+        time_until_next_step = self.m.opt.timestep - (time.time() - step_start)
+        if time_until_next_step > 0:
+          time.sleep(time_until_next_step)
+
+if __name__ == '__main__':
+  r = SimulatedRobotClass()
+  r.run()
