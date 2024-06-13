06a715198f9749656cfbfcf51f28bcf8026b5474
a43414786 <a43414786@gmail.com>
2024-06-13 11:30:53 +0800

    Fix same position-xpos bug

--- simulation.py
+++ simulation.py
@@ -22,6 +22,25 @@ def random_gen_pos():
 cnt = 0
 workspace = []
   
+
+# with open('workspace.pkl', 'rb') as f:
+#   workspace = pkl.load(f)
+
+# init_pos_idx = np.random.randint(0, len(workspace))
+# target_xpos_idx = np.random.randint(0, len(workspace))
+
+
+# init_pos = workspace[init_pos_idx]['pos']
+# target_xpos = workspace[target_xpos_idx]['TCP']
+
+# for i in workspace:
+#   print(i['pos'], i['TCP'])
+  
+# exit()
+
+# print(init_pos_idx, init_pos)
+# print(target_xpos_idx, target_xpos)
+
 with mujoco.viewer.launch_passive(m, d) as viewer:
   start = time.time()
   while viewer.is_running():
@@ -36,15 +55,25 @@ with mujoco.viewer.launch_passive(m, d) as viewer:
           }
         )
       
-      with open('workspace.pkl', 'wb') as f:
-        pkl.dump(workspace, f)
-        if(len(workspace) == 100):
-          exit()
-        
+        with open('workspace.pkl', 'wb') as f:
+          print(workspace[-1])
+          pkl.dump(workspace, f)
+          if(len(workspace) == 100):
+            exit()
+          
       cnt = 0
       pos = random_gen_pos()
-    r.set_target_pos(pos)
     cnt += 1
+    r.set_target_pos(pos)
+    
+    
+    # if(cnt < 1000):
+    #   r.set_target_pos(init_pos)
+    # else:
+    #   r.set_target_pos(r.inverse_kinematics(target_xpos))
+    # cnt += 1
+    # print(cnt,end = '\r')
+    
     
     step_start = time.time()
     mujoco.mj_step(m, d)
--- simulation/interface.py
+++ simulation/interface.py
@@ -69,10 +69,14 @@ class SimulatedRobot:
         :param ee_target_pos: numpy array of target end effector position
         :param joint_name: name of the end effector joint
         """
-        
-        joint_id = self.m.body(joint_name).id
-        # get the current end effector position
-        ee_pos = self.d.geom_xpos[joint_id]
+        if(joint_name=='TCP'):
+            joint_id = self.d.site('TCP').id
+            # get the current end effector position
+            ee_pos = self.d.site('TCP').xpos
+        else:
+            joint_id = self.m.body(joint_name).id
+            # get the current end effector position
+            ee_pos = self.d.geom_xpos[joint_id]
         # compute the jacobian
         jac = np.zeros((3, self.m.nv))
         mujoco.mj_jacBodyCom(self.m, self.d, jac, None, joint_id)
