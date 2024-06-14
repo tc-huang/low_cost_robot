210411ffb61b03d15a014c2c83f5cbe22df46ea0
huang-tc <tzu.chun.huang.tw@gmail.com>
2024-06-14 22:47:15 +0800

    refactor: Update initial_position calculation in experiment_real_robot.py

--- experiment_real_robot.py
+++ experiment_real_robot.py
@@ -1,12 +1,15 @@
 from robot import Robot
 from dynamixel import Dynamixel
 
+def rad2step(rad):
+    return int((rad / 3.14 + 1) * 2048)
+
 
 baudrate=57600
 follower_dynamixel = Dynamixel.Config(baudrate=baudrate, device_name='/dev/tty.usbmodem578E0212131').instantiate()
 follower = Robot(follower_dynamixel, servo_ids=[1, 2, 3, 4, 5, 6])
 
-initial_position = [int(4096 / 2)] * 6
+initial_position = [rad2step(0.0)] * 6
 print(f"Inital position: {initial_position}")
 follower.set_goal_pos(initial_position)
 
